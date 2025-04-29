abd07297d9fcfad787dad5896a775032cf03005c
kavinda1996 <gayashankavinda95@gmail.com>
2025-04-29 10:50:24 +0530

    repository interface

---
+++ src/main/java/edu/icet/ecom/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package edu.icet.ecom.repository;
+
+import edu.icet.ecom.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+
+}
