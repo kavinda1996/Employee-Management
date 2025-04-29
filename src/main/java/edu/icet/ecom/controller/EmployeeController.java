32762eb965b43ea4f550d9c3047d5c14c601727e
kavinda1996 <gayashankavinda95@gmail.com>
2025-04-29 11:01:31 +0530

    post mapping

---
+++ src/main/java/edu/icet/ecom/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package edu.icet.ecom.controller;
+
+import edu.icet.ecom.dto.Employee;
+import edu.icet.ecom.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
+@RequiredArgsConstructor
+@CrossOrigin
+public class EmployeeController {
+
+final EmployeeService service;
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+public void addEmployee(@RequestBody Employee employee){
+    service.addEmployee(employee);
+    }
+}
